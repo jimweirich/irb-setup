a582204cdf3fb51f0c581709b92fe8a160ed101e
Jim Weirich <jim.weirich@gmail.com>
2010-09-20 01:05:58 -0400

    Improved find method

--- find_method.rb
+++ find_method.rb
@@ -10,7 +10,7 @@ def find_method(method_name)
   method_name = Regexp.new(Regexp.quote(method_name.to_s)) if method_name.kind_of?(Symbol)
   method_name = Regexp.new(method_name) if method_name.kind_of?(String)
   ObjectSpace.each_object(Class) do |klass|
-    methods = klass.instance_methods.select { |name| name =~ method_name }
+    methods = klass.instance_methods(false).select { |name| name =~ method_name }
     methods.each do |name|
       result << "#{klass}##{name}"
     end
