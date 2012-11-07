1de49c14203dc14a9dc6899fbe27fd0ddc92d54a
Jim Weirich <jim.weirich@gmail.com>
2012-11-07 10:24:37 -0500

    Make Ripper and parse method optional

--- parsing.rb
+++ parsing.rb
@@ -1,12 +1,18 @@
 if RUBY_VERSION >= "1.9"
-  require 'ripper'
-  require 'pp'
+  begin
+    require 'ripper'
+  rescue LoadError => ex
+  end
 
-  def parse(string)
-    sexp = Ripper::SexpBuilder.new(string).parse
-    sexp = sexp[1] if sexp.first == :program
-    sexp = sexp[2] if sexp.first == :stmts_add
-    pp sexp
-    nil
+  if defined?(Ripper)
+    require 'pp'
+
+    def parse(string)
+      sexp = Ripper::SexpBuilder.new(string).parse
+      sexp = sexp[1] if sexp.first == :program
+      sexp = sexp[2] if sexp.first == :stmts_add
+      pp sexp
+      nil
+    end
   end
 end
